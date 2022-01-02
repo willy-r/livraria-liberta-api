577044259e160c7af5e432186be38ce720b7f81f
William Rodrigues <williamrodrigues2442@gmail.com>
2022-01-02 16:51:55 -0300

    Usa mysql.createPool() para criar conexoes

--- src/infra/dbConexao.js
+++ src/infra/dbConexao.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-const dbConexao = mysql.createConnection({
+const dbConexao = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
--- src/server.js
+++ src/server.js
@@ -1,20 +1,13 @@
 require('dotenv').config();
 
 const customApp = require('./config/customApp');
-const dbConexao = require('./infra/dbConexao');
 const Tabelas = require('./infra/Tabelas');
 
-dbConexao.connect(err => {
-  if (err) {
-    return console.log('ERRO:', err.message);
-  }
+// Cria as tabelas.
+Tabelas.criaTabelas();
 
-  // Cria as tabelas.
-  Tabelas.criaTabelas();
+// Cria o app e inicia o servidor.
+const app = customApp();
+const PORTA = process.env.PORT || 3000;
 
-  // Cria o app e inicia o servidor.
-  const app = customApp();
-  const PORTA = process.env.PORT || 3000;
-
-  app.listen(PORTA, () => console.log(`Server rodando na PORTA: ⚡${PORTA}`));
-});
+app.listen(PORTA, () => console.log(`Server rodando na PORTA: ⚡${PORTA}`));
